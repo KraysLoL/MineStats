e26261c0124736255949250dc16d4905ed52315c
KraysLoL <51406047+KraysLoL@users.noreply.github.com>
2019-06-08 19:22:09 +0300

    Добавлены ники

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
- <?php  
+<?php  
     
     $params = array ( // Параметры.    В будущем как $key => $value
-        '$uuid' => '№',
+        '$uuid' => 'Игрок',
         'stat.leaveGame' => 'Входов в игру',
         'stat.timeSinceDeath' => 'Время последней смерти',
         'stat.playOneMinute' => 'Игровое время',
@@ -16,6 +16,10 @@
         if (strpos($myfilename, '.json') !== false)   //Проверяем заканчивается ли файл на .json 
             $uuidlist[] = substr($myfilename, 0, -5); //Создаём массив со всеми UUID
 
+    $namefile = ('usernamecache.json'); //Файл с никами и UUID
+    $namestr = file_get_contents($namefile); //Получаем строку из файла
+    $namedata = json_decode($namestr); //Декодируем json строку
+
     foreach ($uuidlist as $uuid)//Из массива UUID выбираем по одному ID
     {
         $jsonfile = $statsdir.'/'.$uuid.'.json'; //Полный путь к файлу
@@ -29,7 +33,7 @@
                 if ($key !== '$uuid') //Если параметр это НЕ uuid 
                     $playerdata[''.$value] = $jsondata->{''.$key}; //Выписываем нужную статистику с названием $key (stat.***)
                 else
-                    $playerdata['№'] = $uuid ; //Если это uuid то просто запишем его а не статистику
+                    $playerdata[''.$value] = $namedata->{$uuid} ; //Если это uuid то запишем ник а не статистику
             }
 
             $stats[]=$playerdata ; //Добавим игрока в массив всей статистики
@@ -38,17 +42,17 @@
 
     usort($stats, create_function('$a, $b', "return \$a['Входов в игру'] > \$b['Входов в игру'];")); // Сортировка будущей таблицы по нужному столбцу
 
- 	//Вывод таблицы
+    //Вывод таблицы
     echo 
-        "<caption>Статистика сервера</caption>
+        '<caption>Статистика сервера</caption>
         
         <table>
-            <tr>";
+            <tr>';
     
     foreach($params  as  $key => $value) // По нужным параметрам создаются стобцы таблицы
         echo "<td> $value </td>"; 
 
-    echo "</tr>";
+    echo '</tr>';
 
     foreach($stats as $key => $Player) //Перебираем каждого игрока и создаём строку
     {
