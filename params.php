d115d13617690704a2185a6875d41360c712fe07
KraysLoL <51406047+KraysLoL@users.noreply.github.com>
2019-06-10 11:57:42 +0300

    Create params.php

---
+++ params.php
@@ -0,0 +1,16 @@
+<?php  
+    
+    $params = array ( // Параметры.    В будущем как $key => $value
+        '$uuid' => 'Игрок',
+        'stat.deaths' => 'Количество смертей',
+        'stat.timeSinceDeath' => 'Время последней смерти',
+        'stat.playOneMinute' => 'Игровое время',
+        'eff' => 'Мастерство'
+    );
+    
+    $statsdir = './stats'; //Директория со статой
+
+    $namefile = ('usernamecache.json'); //Файл с никами и UUID
+
+    $sort = 'Мастерство'; //Сортировка по..
+?>
